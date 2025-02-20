ffe2531ee85013d078baba813227f8074722ac3f
Vitaliy <vitaliyypalamarchuck@gmail.com>
2025-02-20 16:17:52 +0200

    Виправлення помилки в алгоритмі №4 та Прописання алгоритмів обернених до даних в методичці

---
+++ src/main/java/BasicAlgSortReversed.java
@@ -0,0 +1,94 @@
+
+import java.util.Arrays;
+
+
+public class BasicAlgSortReversed <T extends Comparable<? super T>>{
+
+    private int left;
+    private int right;
+    private T[] array;
+
+    public BasicAlgSortReversed(T[] array){ this.array=Arrays.copyOf(array, array.length);}
+
+    public BasicAlgSortReversed(int left, int right, T[] array) {
+        this.left = left;
+        this.right = right;
+        this.array = Arrays.copyOfRange(array,left,right);
+    }
+
+    public T[] getArray(){return array;}
+
+    public void bubbleSortDescReversed() {
+        boolean flagForInteration = true;
+        while (flagForInteration) {
+            flagForInteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i].compareTo(array[i - 1]) > 0) { // Замінив < на >
+                    swap(i, i - 1);
+                    if (!flagForInteration) {
+                        flagForInteration = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void swap(int leftIndex, int rightIndex) {
+        T temp = array[leftIndex];
+        array[leftIndex] = array[rightIndex];
+        array[rightIndex]=temp;
+    }
+    public void selectionSortAscReversed(){
+        for (int i = 0; i < array.length; i++) {
+            int maxIndex=i; //всюди замінив minIndex на maxIndex та min на max
+            T max=array[i];
+            for (int j = i+1; j < array.length; j++) {
+                if(array[j].compareTo(max)>0){ // Замінив < на >
+                    max =array[j];
+                    maxIndex=j;
+                }
+            }
+            if (i!=maxIndex) swap(i,maxIndex);
+        }
+    }
+
+    public void insertionSortWithLinearSearchAscReversed(){
+        for(int i=1; i<array.length; i++){
+            T key =array[i];
+            int j = i;
+            for (; j>0;j--){
+                if(key.compareTo(array[j-1])>0){ // Замінив < на >
+                    array[j]=array[j-1];
+                }else {
+                    break;
+                }
+            }
+            array[j]=key;
+        }
+    }
+
+    public void insertionSortWithBinarySearchAscReversed(){
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int leftIndex = 0;
+            int rightIndex = i-1;
+            if(key.compareTo(array[i-1])>0){ // Замінив < на >
+                while (leftIndex <= rightIndex){
+                    int serIndex=(rightIndex+leftIndex)/2;
+                    if(key.compareTo(array[serIndex])>0){ // Замінив < на >
+                        rightIndex=serIndex-1; // додав -1
+                    }else{
+                        leftIndex=serIndex+1;
+                    }
+                }
+                for(int j = i; j >leftIndex ; j--) {
+                    array[j]=array[j-1];
+                }
+                array[leftIndex]=key;
+            }
+
+        }
+    }
+
+
+}
---
+++ src/test/java/BasicAlgSortReversedTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BasicAlgSortReversedTest {
+
+    @Test
+    public void testBubbleSortWhenArraysWithIntegerTypesAndRangeEqualsLengthReversed(){
+        Integer [] sourceArray = new Integer[]{5,4,10,2,1};
+        Integer [] destArray = new Integer[]{10,5,4,2,1};
+        BasicAlgSortReversed<Integer> integerBasicAlgSort = new BasicAlgSortReversed<>(sourceArray);
+        integerBasicAlgSort.bubbleSortDescReversed();
+        assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
+    }
+
+    @Test
+    public void testselectionSortWhenArraysWithIntegerTypesAndRangeEqualsLengthReversed() {
+        Integer [] sourceArray = new Integer[]{5,4,10,2,1};
+        Integer [] destArray = new Integer[]{10,5,4,2,1};
+        BasicAlgSortReversed<Integer> integerBasicAlgSort = new BasicAlgSortReversed<>(sourceArray);
+        integerBasicAlgSort.selectionSortAscReversed();
+        assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
+    }
+
+    @Test
+    public void testInsertionSortWhenArraysWithIntegerTypesAndRangeEqualsLengthReversed() {
+        Integer [] sourceArray = new Integer[]{5,4,10,2,1,8,-5,0};
+        Integer [] destArray = new Integer[]{10,8,5,4,2,1,0,-5};
+        BasicAlgSortReversed<Integer> integerBasicAlgSort = new BasicAlgSortReversed<>(sourceArray);
+        integerBasicAlgSort.insertionSortWithLinearSearchAscReversed();
+        assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
+    }
+    @Test
+    public void testInsertionSortWithBinaryWhenArraysWithIntegerTypesAndRangeEqualsLengthReversed() {
+        Integer [] sourceArray = new Integer[]{5,4,10,2,1,8,-5,0};
+        Integer [] destArray = new Integer[]{10,8,5,4,2,1,0,-5};
+        BasicAlgSortReversed<Integer> integerBasicAlgSort = new BasicAlgSortReversed<>(sourceArray);
+        integerBasicAlgSort.insertionSortWithBinarySearchAscReversed();
+        assertThat(integerBasicAlgSort.getArray()).isEqualTo(destArray);
+    }
+
+
+    public static Integer[] getRandomInteger(int n){
+        Integer [] sourceArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            sourceArray[i]=new Random().nextInt(200)-100;
+        }
+        return sourceArray;
+    }
+
+
+
+
+}
