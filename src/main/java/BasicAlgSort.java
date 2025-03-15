3db553073535186c4cf50cf5c0f9b510a6179095
Vitaliy <vitaliyypalamarchuck@gmail.com>
2025-03-15 12:28:49 +0200

    Додав порівняння часу

--- src/main/java/BasicAlgSort.java
+++ src/main/java/BasicAlgSort.java
@@ -8,19 +8,14 @@ public class BasicAlgSort <T extends Comparable<? super T>>{
     private int right;
     private T[] array;
     boolean SortingFlag;
-
-    public BasicAlgSort(T[] array, boolean sortingFlag) {
-        this.array = Arrays.copyOf(array, array.length);
-        this.SortingFlag = sortingFlag;
-    }
-
+    public BasicAlgSort(T[] array){ this.array=Arrays.copyOf(array, array.length);}
 
 
     public BasicAlgSort(int left, int right, T[] array, boolean sortingFlag) {
         this.left = left;
         this.right = right;
         this.array = array;
-        this.SortingFlag = sortingFlag;
+        SortingFlag = sortingFlag;
     }
 
     public T[] getArray(){return array;}
@@ -30,7 +25,7 @@ public class BasicAlgSort <T extends Comparable<? super T>>{
         while (flagForInteration){
             flagForInteration=false;
             for (int i =1; i<array.length;i++){
-                if (SortingFlag ? array[i].compareTo(array[i - 1]) > 0 : array[i].compareTo(array[i - 1]) < 0){
+                if(SortingFlag ? array[i].compareTo(array[i - 1]) > 0 : array[i].compareTo(array[i - 1]) < 0){
                     swap(i, i-1);
                     if(!flagForInteration){
                         flagForInteration=true;
@@ -60,41 +55,53 @@ public class BasicAlgSort <T extends Comparable<? super T>>{
             }
         }
     }
-
-    public void insertionSortWithLinearSearchAsc() {
-            for (int i = 1; i < array.length; i++) {
-                T key = array[i];
-                int j = i;
-                while (j > 0 && (SortingFlag ? key.compareTo(array[j - 1]) > 0 : key.compareTo(array[j - 1]) < 0)) {
-                    array[j] = array[j - 1];
-                    j--;
-                }
-                array[j] = key;
+    public void insertionSortWithLinearSearch() {
+        long startTime=System.currentTimeMillis();
+        for (int i = 1; i < array.length; i++) {
+            T key = array[i];
+            int j = i;
+            while (j > 0 && (SortingFlag ? key.compareTo(array[j - 1]) > 0 : key.compareTo(array[j - 1]) < 0)) {
+                array[j] = array[j - 1];
+                j--;
             }
+            array[j] = key;
+        }
+
+        long endTime=System.currentTimeMillis();
+        long time =endTime-startTime;
+        System.out.println("Час виконання insertionSortWithLinearSearch: " + time + " мс");
     }
 
-    public void insertionSortWithBinarySearchAsc(){
+
+    public void insertionSortWithBinarySearchAsc() {
+        long startTime=System.currentTimeMillis();
+
         for (int i = 1; i < array.length; i++) {
             T key = array[i];
             int leftIndex = 0;
-            int rightIndex = i-1;
-            if (SortingFlag ? key.compareTo(array[i-1]) > 0 : key.compareTo(array[i-1]) < 0) {
-                while (leftIndex <= rightIndex){
-                    int serIndex=(rightIndex+leftIndex)/2;
-                    if(SortingFlag? key.compareTo(array[serIndex])>0:key.compareTo(array[serIndex])<0){
-                        rightIndex=serIndex-1;
-                    }else{
-                        leftIndex=serIndex+1;
+            int rightIndex = i - 1;
+            if (SortingFlag ? key.compareTo(array[i - 1]) > 0 : key.compareTo(array[i - 1]) < 0) {
+                while (leftIndex <= rightIndex) {
+                    int serIndex = (rightIndex + leftIndex) / 2;
+                    if (SortingFlag ? key.compareTo(array[serIndex]) > 0 : key.compareTo(array[serIndex]) < 0) {
+                        rightIndex = serIndex - 1;
+                    } else {
+                        leftIndex = serIndex + 1;
                     }
                 }
-
-                for(int j = i; j >leftIndex ; j--) {
-                    array[j]=array[j-1];
+                for (int j = i; j > leftIndex; j--) {
+                    array[j] = array[j - 1];
                 }
-                array[leftIndex]=key;
+                array[leftIndex] = key;
             }
-
         }
+        long endTime=System.currentTimeMillis();
+        long time =endTime-startTime;
+        System.out.println("Час виконання insertionSortWithBinarySearchAsc: " + time + " мс");
+
     }
+
+
+
 
 }
